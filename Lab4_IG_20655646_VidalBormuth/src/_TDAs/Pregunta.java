bf7971a5a59c69d08f95a05cd680061e97d4cef8
Alma Vidal <alma.vidal@usach.cl>
2021-02-26 21:16:41 -0400

    Nuevas clases.

---
+++ Lab4_IG_20655646_VidalBormuth/src/_TDAs/Pregunta.java
@@ -0,0 +1,138 @@
+package _TDAs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+/**
+ * Clase para representar una pregunta en el stack.
+ * La clase pregunta hereda de la clase ComunRP.
+ * La clase pregunta queda determinada por un incrementador de id, un titulo, una recompensa, una lista de etiquetas, una lista de respuestas
+ * y de los atributos heredados: un id único incremental, un autor, contenido, votos a favor, votos en contra, reportes,
+ * un estado y una fecha de publicación.
+ * @author Alma
+ *
+ */
+
+public class Pregunta extends ComunRP{
+	
+	private static int count2 = 0; //Incrementador del id de la clase Pregunta.
+	private String titulo; //Un titulo.
+	private Recompensa recompensa; //Una recompensa.
+	private List<Etiqueta> etiquetas; //Una lista de etiquetas.
+	private List<Respuesta> respuestas; //Una lista de respuestas.
+
+	
+	/**
+	 * Crea una nueva pregunta a partir de un autor, un titulo, contenido y una lista de etiquetas.
+	 * @param autor Autor de la pregunta.
+	 * @param titulo Titulo de la pregunta.
+	 * @param contenido Contenido de la pregunta.
+	 * @param etiquetas Lista de etiquetas de la pregunta.
+	 */
+	public Pregunta(String autor, String titulo, String contenido, List<Etiqueta> etiquetas) {
+		super(autor, contenido);
+		this.titulo = titulo;
+		setEstado("Abierta."); //Al crear la respuesta su estado pasa a ser "Abierta."
+		this.etiquetas = etiquetas;
+		respuestas = new ArrayList<Respuesta>(); //Se inicializa la lista de preguntas.
+		recompensa = new Recompensa(); //Se inicializa una recompensa vacía.
+		setId(++count2); //El id autoincremental se incrementa.
+	}
+
+	/**
+	 * Crea una nueva pregunta a partir de todos sus atributos a excepción del id único incremental y la fecha de publicación.
+	 * @param autor Autor de la pregunta
+	 * @param titulo Titulo de la pregunta.
+	 * @param contenido Contenido de la pregunta.
+	 * @param etiquetas Lista de etiquetas de la pregunta.
+	 * @param estado Estado de la pregunta ("Abierta." o "Cerrada.").
+	 * @param votosAFavor Cantidad de votos a favor de la pregunta.
+	 * @param votosEnContra Cantidad de votos en contra de la pregunta.
+	 * @param reportes Cantidad de reportes de la pregunta.
+	 * @param recompensa Recompensa por la pregunta.
+	 * @param respuestas Lista de respuestas de la pregunta.
+	 */
+	public Pregunta(String autor, String titulo, String contenido, List<Etiqueta> etiquetas, String estado, int votosAFavor, 
+			int votosEnContra, int reportes, Recompensa recompensa, List<Respuesta> respuestas) {
+		super(autor, contenido, estado, votosAFavor, votosEnContra, reportes);
+		this.titulo = titulo;
+		this.recompensa = recompensa;
+		this.etiquetas = etiquetas;
+		this.respuestas = respuestas;
+		setId(++count2); //El id autoincremental se incrementa.
+	}
+
+
+	/**
+	 * Entrega el titulo de la pregunta.
+	 * @return Titulo de la pregunta.
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+
+	/**
+	 * Permite modificar el titulo de la pregunta.
+	 * @param titulo Titulo de la pregunta.
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	/**
+	 * Entrega la recompensa por la pregunta.
+	 * @return La recompensa de la pregunta.
+	 */
+	public Recompensa getRecompensa() {
+		return recompensa;
+	}
+	
+	/**
+	 * Permite modificar la recompensa de la pregunta.
+	 * @param recompensa Recompensa.
+	 */
+	public void setRecompensa(Recompensa recompensa) {
+		this.recompensa = recompensa;
+	}
+
+	/**
+	 * Entrega la lista de etiquetas de una pregunta.
+	 * @return Lista de etiquetas de la pregunta.
+	 */
+	public List<Etiqueta> getEtiquetas() {
+		return etiquetas;
+	}
+
+	/**
+	 * Permite modificar la lista de etiquetas de la pregunta.
+	 * @param etiquetas Lista de etiquetas.
+	 */
+	public void setEtiquetas(List<Etiqueta> etiquetas) {
+		this.etiquetas = etiquetas;
+	}
+
+	/**
+	 * Entrega la lista de etiquetas de la pregunta.
+	 * @return Lista de preguntas de la etiqueta.
+	 */
+	public List<Respuesta> getRespuestas() {
+		return respuestas;
+	}
+
+	/**
+	 * Permite modificar la lista de respuestas de la pregunta. 
+	 * @param respuestas Una lista de respuestas.
+	 */
+	public void setRespuestas(List<Respuesta> respuestas) {
+		this.respuestas = respuestas;
+	}
+	
+
+	
+
+	
+}
---
+++ Lab4_IG_20655646_VidalBormuth/src/_TDAs/Stack.java
@@ -0,0 +1,92 @@
+package _TDAs;
+
+import java.util.List;
+
+
+
+/**
+ * Clase que representa un stack.
+ * Esta clase esta determinada por un id único incremental, una lista de usuarios, una lista de preguntas y una lista de etiquetas. 
+ * @author Alma
+ *
+ */
+public class Stack  {
+	
+	private List<Usuario> usuarios; //Lista de usuarios.
+	private List<Pregunta> preguntas; //Lista de preguntas.
+	private List<Etiqueta> etiquetas; //Lista de etiquetas. 
+	private Usuario activeUser = null;
+	/**
+	 * Crea un nuevo stack a partir de una lista de usuarios, una lista de preguntas y una lista de etiquetas. 
+	 * @param usuarios Lista de usuarios.
+	 * @param preguntas Lista de preguntas.
+	 * @param etiquetas Lista de etiquetas. 
+	 */
+	public Stack(List<Usuario> usuarios, List<Pregunta> preguntas, List<Etiqueta> etiquetas) {
+		this.usuarios = usuarios;
+		this.preguntas = preguntas;
+		this.etiquetas = etiquetas;
+
+	}
+
+	/**
+	 * Permite obtener la lista de usuarios del stack.
+	 * @return Lista de usuarios.
+	 */
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+	/**
+	 * Permite modificar la lista de usuarios del stack.
+	 * @param usuarios Lista de usuarios. 
+	 */
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	/**
+	 * Entraga la lista de preguntas del stack.
+	 * @return Lista de preguntas.
+	 */
+	public List<Pregunta> getPreguntas() {
+		return preguntas;
+	}
+	/**
+	 * Permite modificar las lista de preguntas del stack.
+	 * @param preguntas Nueva lista de preguntas. 
+	 */
+	public void setPreguntas(List<Pregunta> preguntas) {
+		this.preguntas = preguntas;
+	}
+	/**
+	 * Entrega la lista de etiquetas del stack.
+	 * @return Lista de etiquetas del stack.
+	 */
+	public List<Etiqueta> getEtiquetas() {
+		return etiquetas;
+	}
+	/**
+	 * Permite modificar la lista de etiquetas del stack.
+	 * @param etiquetas Nueva lista de etiquetas. 
+	 */
+	public void setEtiquetas(List<Etiqueta> etiquetas) {
+		this.etiquetas = etiquetas;
+	}
+
+	
+	
+	public Usuario getActiveUser() {
+		return activeUser;
+	}
+
+	public void setActiveUser(Usuario activeUser) {
+		this.activeUser = activeUser;
+	}
+	
+	
+
+	
+	
+
+
+	
+}
