10902b80b4bb296ba0ca6e3384afa8c7c451c931
Alma Vidal <alma.vidal@usach.cl>
2021-02-27 18:22:37 -0400

    Avance interfaz.

--- Lab4_IG_20655646_VidalBormuth/src/interfaz/Inicio.java
+++ Lab4_IG_20655646_VidalBormuth/src/interfaz/Inicio.java
@@ -54,31 +54,36 @@ public class Inicio {
 		frame.setFont(new Font("Dialog", Font.PLAIN, 36));
 		frame.setBounds(100, 100, 1194, 488);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Bienvenido a StackOverflow!!");
+		lblNewLabel.setBounds(0, 29, 1178, 73);
 		lblNewLabel.setToolTipText("");
 		lblNewLabel.setForeground(Color.BLACK);
-		lblNewLabel.setFont(new Font("Mongolian Baiti", Font.BOLD, 62));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 60));
 		lblNewLabel.setBackground(Color.BLACK);
-		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		frame.getContentPane().add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Login");
+		JButton btnNewButton = new JButton("Ingresar");
+		btnNewButton.setBounds(10, 113, 164, 48);
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		btnNewButton.setForeground(UIManager.getColor("InternalFrame.activeTitleForeground"));
 		btnNewButton.setBackground(UIManager.getColor("InternalFrame.activeTitleBackground"));
-		frame.getContentPane().add(btnNewButton, BorderLayout.WEST);
+		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Registarse");
+		btnNewButton_1.setBounds(10, 183, 164, 48);
 		btnNewButton_1.setForeground(UIManager.getColor("InternalFrame.activeTitleForeground"));
 		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 13));
 		btnNewButton_1.setBackground(UIManager.getColor("InternalFrame.activeTitleBackground"));
-		frame.getContentPane().add(btnNewButton_1, BorderLayout.CENTER);
+		frame.getContentPane().add(btnNewButton_1);
 		
 		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(0, 449, 1178, 0);
 		layeredPane.setForeground(Color.WHITE);
 		layeredPane.setBorder(new CompoundBorder());
 		layeredPane.setBackground(Color.WHITE);
-		frame.getContentPane().add(layeredPane, BorderLayout.SOUTH);
+		frame.getContentPane().add(layeredPane);
 		layeredPane.setLayout(new BoxLayout(layeredPane, BoxLayout.X_AXIS));
 		
 		
